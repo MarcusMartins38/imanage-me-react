c178a17f96e4e98997d01b49395a28db6248fe3b
unknown <marcusmartins38@gmail.com>
2024-12-25 19:05:07 -0400

    remove: task mock

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -7,33 +7,6 @@ import { RootState } from "@reduxjs/toolkit/query";
 import CreateTaskModal from "../components/CreateTaskModal";
 import { useCookies } from "react-cookie";
 
-const mockTasks = [
-  {
-    id: 1,
-    title: "Study Time",
-    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-              officia unde accusamus voluptatum magnam consequuntur enim,
-              officiis minima perferendis? Dolore nostrum in architecto
-              repellendus? Maxime ipsum ipsam distinctio eligendi suscipit!`,
-  },
-  {
-    id: 2,
-    title: "Study Time 2",
-    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-              officia unde accusamus voluptatum magnam consequuntur enim,
-              officiis minima perferendis? Dolore nostrum in architecto
-              repellendus? Maxime ipsum ipsam distinctio eligendi suscipit!`,
-  },
-  {
-    id: 3,
-    title: "Study Time 3",
-    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-              officia unde accusamus voluptatum magnam consequuntur enim,
-              officiis minima perferendis? Dolore nostrum in architecto
-              repellendus? Maxime ipsum ipsam distinctio eligendi suscipit!`,
-  },
-];
-
 function Tasks() {
   const [tasks, setTasks] = useState([]);
   const [cookies] = useCookies(["userAuth"]);
