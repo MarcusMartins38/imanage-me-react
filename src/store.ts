11ff2e51ba99f9633a6b950255d17ad6bcab7078
Marcus Martins <marcusmartins38@gmail.com>
2025-01-04 18:31:40 -0400

    fix: redux user slice

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sidebarReducer from "./slices/sidebarSlice";
-import userReducer from "./slices/sidebarSlice";
+import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
