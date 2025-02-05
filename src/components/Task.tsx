448ae876d4d78ba908f62c266ba9a4abd748f821
unknown <marcusmartins38@gmail.com>
2025-02-04 22:52:02 -0400

    refactor: pass icon to a component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,6 +4,8 @@ import { PRIORITIES } from "../lib/constants";
 import { useCookies } from "react-cookie";
 import { useFieldArray, useForm } from "react-hook-form";
 import SubTask from "./SubTask";
+import EditIcon from "../assets/icons/EditIcon";
+import TrashIcon from "../assets/icons/TrashIcon";
 
 type TaskProps = {
   task: TaskT;
@@ -179,47 +181,13 @@ const Task: React.FC<TaskProps> = ({
             onClick={() => setIsEditOpen((prev) => !prev)}
             className="btn bg-base-300 border-base-300 min-h-0 h-10 w-10 p-0 hover:bg-blue-500 hover:text-white"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-edit"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
-              <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
-              <path d="M16 5l3 3" />
-            </svg>
+            <EditIcon />
           </button>
           <button
             onClick={() => handleRemoveTask(task.id)}
             className="btn bg-base-300 border-base-300 min-h-0 h-10 w-10 p-0 hover:bg-red-500 hover:text-white"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-trash"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M4 7l16 0" />
-              <path d="M10 11l0 6" />
-              <path d="M14 11l0 6" />
-              <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-              <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-            </svg>
+            <TrashIcon />
           </button>
         </section>
       </div>
