db77bebc766a00bd0e84a0d61c8b4d9226b91fef
unknown <marcusmartins38@gmail.com>
2025-02-19 22:27:54 -0400

    feat: fix style & create icon component file

---
+++ src/assets/icons/TaskIcon.tsx
@@ -0,0 +1,22 @@
+export default function TaskIcon({ isOpen = true }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={`icon icon-tabler icons-tabler-outline icon-tabler-checklist ${isOpen ? "mr-1" : "m-auto"}`}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M9.615 20h-2.615a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8" />
+      <path d="M14 19l2 2l4 -4" />
+      <path d="M9 8h4" />
+      <path d="M9 12h2" />
+    </svg>
+  );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,6 +6,13 @@ import { closeSidebar, openSidebar } from "../slices/sidebarSlice";
 import { updateUser } from "../slices/userSlice";
 import { changeTheme } from "../slices/uiSlice";
 import { useEffect } from "react";
+import AboutMeIcon from "../assets/icons/AboutMeIcon";
+import TaskIcon from "../assets/icons/TaskIcon";
+import ArrowRight from "../assets/icons/ArrowRight";
+import ArrowLeft from "../assets/icons/ArrowLeft";
+import LogoutIcon from "../assets/icons/LogoutIcon";
+import LightThemeIcon from "../assets/icons/LightThemeIcon";
+import DarkThemeIcon from "../assets/icons/DarkThemeIcon";
 
 const Sidebar = () => {
   const isOpen = useSelector((state: RootState) => state.sidebar.isOpen);
@@ -27,7 +34,6 @@ const Sidebar = () => {
   };
 
   const handleLogoutClick = () => {
-    const res = fetch("");
     dispatch(updateUser({ name: "", email: "", imageUrl: "" }));
     removeCookie("userAuth");
     navigate("/login");
@@ -48,78 +54,36 @@ const Sidebar = () => {
           borderRadius: "0 24px 24px 0",
         }}
       >
-        <section className="p-2">
-          <button
-            onClick={handleToggleClick}
-            className="btn btn-ghost w-full py-2 px-0"
-          >
-            {isOpen ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="icon icon-tabler icons-tabler-filled icon-tabler-square-rounded-arrow-left"
+        <div className="menu p-2 !pt-0 h-full flex flex-col justify-between">
+          <div>
+            <section className="p-2">
+              <button
+                onClick={handleToggleClick}
+                className="btn btn-ghost w-full py-2 px-0"
               >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path
-                  d="M12 2l.324 .001l.318 .004l.616 .017l.299 .013l.579 .034l.553 .046c4.785 .464 6.732 2.411 7.196 7.196l.046 .553l.034 .579c.005 .098 .01 .198 .013 .299l.017 .616l.005 .642l-.005 .642l-.017 .616l-.013 .299l-.034 .579l-.046 .553c-.464 4.785 -2.411 6.732 -7.196 7.196l-.553 .046l-.579 .034c-.098 .005 -.198 .01 -.299 .013l-.616 .017l-.642 .005l-.642 -.005l-.616 -.017l-.299 -.013l-.579 -.034l-.553 -.046c-4.785 -.464 -6.732 -2.411 -7.196 -7.196l-.046 -.553l-.034 -.579a28.058 28.058 0 0 1 -.013 -.299l-.017 -.616c-.003 -.21 -.005 -.424 -.005 -.642l.001 -.324l.004 -.318l.017 -.616l.013 -.299l.034 -.579l.046 -.553c.464 -4.785 2.411 -6.732 7.196 -7.196l.553 -.046l.579 -.034c.098 -.005 .198 -.01 .299 -.013l.616 -.017c.21 -.003 .424 -.005 .642 -.005zm.707 5.293a1 1 0 0 0 -1.414 0l-4 4a1.037 1.037 0 0 0 -.2 .284l-.022 .052a.95 .95 0 0 0 -.06 .222l-.008 .067l-.002 .063v-.035v.073a1.034 1.034 0 0 0 .07 .352l.023 .052l.03 .061l.022 .037a1.2 1.2 0 0 0 .05 .074l.024 .03l.073 .082l4 4l.094 .083a1 1 0 0 0 1.32 -.083l.083 -.094a1 1 0 0 0 -.083 -1.32l-2.292 -2.293h5.585l.117 -.007a1 1 0 0 0 -.117 -1.993h-5.585l2.292 -2.293l.083 -.094a1 1 0 0 0 -.083 -1.32z"
-                  fill="currentColor"
-                  strokeWidth="0"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-square-rounded-arrow-right"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M12 16l4 -4l-4 -4" />
-                <path d="M8 12h8" />
-                <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
-              </svg>
-            )}
-          </button>
-        </section>
-
-        <div className="menu p-2 !pt-0 h-full">
-          <NavLink
-            to="/tasks"
-            className="menu-item flex items-center w-full h-10 rounded-lg px-1 transition-all duration-300 hover:bg-base-300"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className={`icon icon-tabler icons-tabler-outline icon-tabler-checklist ${isOpen ? "mr-1" : "m-auto"}`}
+                {isOpen ? <ArrowLeft /> : <ArrowRight />}
+              </button>
+            </section>
+            <NavLink
+              to="/tasks"
+              className="menu-item flex items-center w-full h-10 rounded-lg px-1 transition-all duration-300 hover:bg-base-300"
             >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M9.615 20h-2.615a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8" />
-              <path d="M14 19l2 2l4 -4" />
-              <path d="M9 8h4" />
-              <path d="M9 12h2" />
-            </svg>
-            {isOpen && <span>Tasks</span>}
-          </NavLink>
+              <TaskIcon isOpen={isOpen} />
+              {isOpen && <span>Tasks</span>}
+            </NavLink>
 
-          <div className="absolute mb-2 bottom-0 w-full">
+            <NavLink
+              to="/about"
+              className="menu-item flex items-center w-full h-10 rounded-lg px-1 transition-all duration-300 hover:bg-base-300"
+            >
+              <AboutMeIcon isOpen={isOpen} />
+              {isOpen && <span className="ml-1">About Me</span>}
+            </NavLink>
+          </div>
+
+          <div className="mb-2 bottom-0 w-full">
             <label
-              className={`menu-item justify-start w-full h-10 swap swap-rotate rounded-lg px-1 transition-all ${isOpen ? "max-w-60" : "max-w-12 justify-center"} duration-300 hover:bg-base-300`}
+              className={`menu-item justify-start w-full h-10 swap swap-rotate rounded-lg px-[3px] transition-all flex items-center ${isOpen ? "max-w-60" : "max-w-12 justify-center"} duration-300 hover:bg-base-300`}
             >
               <input
                 type="checkbox"
@@ -127,24 +91,9 @@ const Sidebar = () => {
                 checked={theme === "light"}
                 onChange={handleThemeToggle}
               />
+              {theme === "dark" ? <LightThemeIcon /> : <DarkThemeIcon />}
 
-              {theme === "dark" ? (
-                <svg
-                  className="swap-off h-7 w-7 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-                </svg>
-              ) : (
-                <svg
-                  className="swap-on h-7 w-7 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-                </svg>
-              )}
+              {isOpen && <span className="ml-[2px]">Switch Theme</span>}
             </label>
 
             <NavLink
@@ -175,23 +124,7 @@ const Sidebar = () => {
               onClick={handleLogoutClick}
               className={`menu-item flex items-center w-full h-10 rounded-lg px-1 transition-all group hover:bg-red-500 ${isOpen ? "max-w-60" : "max-w-12"} duration-300`}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className={`icon icon-tabler icons-tabler-outline icon-tabler-logout-2 text-red-600 group-hover:text-white duration-300 transition-all ${!isOpen ? "m-auto ml-[6px]" : "mr-1 ml-[-2px]"}`}
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
-                <path d="M15 12h-12l3 -3" />
-                <path d="M6 15l-3 -3" />
-              </svg>
+              <LogoutIcon isOpen={isOpen} />
               {isOpen && (
                 <span className="text-red-600 group-hover:text-white duration-300 transition-all">
                   Logout
